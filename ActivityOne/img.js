78d3f45217cd4c058291d93c1ebd3da61cef8855
elmer <you@example.com>
2024-03-22 14:24:54 +0800

    elmer

--- ActivityOne/img.js
+++
@@ -1,8 +0,0 @@
-const imagess = (img)=> {
-  let div = document.createElement("div")
-  div.className="rightDiv"
-  div.innerHTML= `<img class="imgs" src="${img}"> `
-  
-  return div
-}
-export{imagess}
